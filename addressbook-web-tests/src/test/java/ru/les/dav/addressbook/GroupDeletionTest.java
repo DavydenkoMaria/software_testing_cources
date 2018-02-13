a802bf13762411e3d3e7b9186d8f9cd4973615df
DavydenkoMaria <saakova.masha@gmail.com>
2018-02-13 15:59:34 +0700

    Создан базовый класс для тестов

---
+++ addressbook-web-tests/src/test/java/ru/les/dav/addressbook/GroupDeletionTest.java
@@ -0,0 +1,15 @@
+package ru.les.dav.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+    
+    @Test
+    public void testGroupDeletion() {
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+}
