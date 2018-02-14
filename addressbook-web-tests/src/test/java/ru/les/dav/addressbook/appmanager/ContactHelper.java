0a7e77adfaff2a477bb3054445783df180c7b799
DavydenkoMaria <saakova.masha@gmail.com>
2018-02-14 14:14:40 +0700

    Создан общий базовый класс BaseHelper

--- addressbook-web-tests/src/test/java/ru/les/dav/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/les/dav/addressbook/appmanager/ContactHelper.java
@@ -7,39 +7,26 @@ import ru.les.dav.addressbook.model.ContactShortData;
 /**
  * Created by saakovamr on 14.02.18.
  */
-public class ContactHelper {
-   private FirefoxDriver wd;
+public class ContactHelper extends BaseHelper {
 
    public ContactHelper(FirefoxDriver wd) {
-      this.wd = wd;
+      super(wd);
    }
 
    public void submitContactCreation() {
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+      click(By.xpath("//div[@id='content']/form/input[21]"));
    }
 
    public void fillContactForm(ContactShortData contactShortData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactShortData.getFirstName());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactShortData.getLastName());
-      wd.findElement(By.name("address")).click();
-      wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(contactShortData.getAddress());
-      wd.findElement(By.name("mobile")).click();
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(contactShortData.getMobileNumber());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactShortData.getEmail());
-      wd.findElement(By.name("title")).click();
-      wd.findElement(By.name("title")).clear();
-      wd.findElement(By.name("title")).sendKeys(contactShortData.getTitle());
+      type(By.name("firstname"),contactShortData.getFirstName());
+      type(By.name("lastname"),contactShortData.getLastName());
+      type(By.name("address"),contactShortData.getAddress());
+      type(By.name("mobile"),contactShortData.getMobileNumber());
+      type(By.name("email"),contactShortData.getEmail());
+      type(By.name("title"),contactShortData.getTitle());
    }
 
    public void initContactCreation() {
-      wd.findElement(By.linkText("add new")).click();
+      click(By.linkText("add new"));
    }
 }
